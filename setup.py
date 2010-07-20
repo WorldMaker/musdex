2e95ac77858697e97cdaadfb84f02ae2f25e1b9c
Max Battcher <me@worldmaker.net>
2010-07-20 00:39:33 +0000

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='musdex',
-    version='10.07.14',
+    version='10.07.19',
     description='musdex -- VCS-aware zip archive tool',
     author='Max Battcher',
     author_email='me@worldmaker.net',
